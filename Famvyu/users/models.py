4459e86c633831119392fab1fc720c14ef42f2c6
oMarquess <pymasterclass@gmail.com>
2023-12-03 21:54:09 +0000

    User model error fixed

--- Famvyu/users/models.py
+++ Famvyu/users/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Movie(models.Model):
-    title = models.Char(max_length = 255)
+    title = models.CharField(max_length = 255)
     
