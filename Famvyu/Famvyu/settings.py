5899ee2404fa55ffa58bf6f68b62272388774136
KofiDakwa <kopokudakwa@gmail.com>
2023-12-17 19:45:15 -0500

    Railway Setup

--- Famvyu/Famvyu/settings.py
+++ Famvyu/Famvyu/settings.py
@@ -101,10 +101,21 @@ WSGI_APPLICATION = 'Famvyu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '1GCE*5baeEdaF1*4D2B364BGb4*2gA-C',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '27453',
     }
 }
 
